1b559d04e9d972a1ad5c1821227d2a86cffb326a
jakirajumafahad <jakirajumafahad@gmail.com>
2026-04-01 15:10:23 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,24 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: JAKIRA JUMA FAHAD */
+/* Student Number: 25/U/BIO/01374/PD */
 
 #include <stdio.h>
 
 int main(void)
 {
-    /* Write your code here */
+     double amount, fee_percent, fee_amount, total_deducted;
+     //Inputs from the user
+    printf("Enter amount to send ");
+    scanf("%lf",&amount);
+    printf("Enter fee_percent (%%):");
+    scanf("%lf",&fee_percent);
+     //Calculate fee amount
+    fee_amount=(fee_percent/100)*amount;
+
+    //Displays the calculated value of the amount
+    printf("Fee Amount   %.2f\n", fee_amount);
+//Amount deducted is calculated by the formula
+    total_deducted=amount+fee_amount;
+     printf("Total Amount Deducted:%.2f\n", total_deducted);
 
     return 0;
 }
